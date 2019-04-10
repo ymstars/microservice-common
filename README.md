9c43b35b3797b7906483505881df8ac308d059bd
ymstars <ymstars@qq.com>
2019-04-10 11:07:19 +0800

    initialize!

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # microservice-common
 microservice common lib for spring cloud
+
+```
+SUCCESS_CODE=0
+
+SUCCESS_MESSAGE="SUCCESS"
+
+```
+
+通过如下方式构造通用响应
+
+```
+//没有数据返回的成功Response
+ResultResponse.SUCCESS();
+
+//有数据返回的成功Response
+ResultResponse.SUCCESS(T data);
+
+//已知错误码的Error Response
+ResultResponse.ERROR(int code,String message);
+
+//未知错误码的Error Response
+ResultResponse.ERROR();
+
+```
+
+> T 类 必须实现`Serializable`接口
---
+++ src/main/java/com/dqx/jq/microservice/common/response/ResultResponse.java
@@ -0,0 +1,79 @@
+package com.dqx.jq.microservice.common.response;
+
+import java.io.Serializable;
+
+/**
+ * Container For MicroServices Transfer
+ *
+ * @param <T>
+ */
+public class ResultResponse<T extends Serializable> implements Serializable {
+
+
+    private static final int SUCCESS_CODE = 0;
+    private static final String SUCCESS_MESSAGE = "SUCCESS";
+    private static final int ERROR_CODE_UNKNOWN = 999999;
+    private static final String ERROR_MESSAGE_UNKNOWN = "UNKOWN ERROR";
+    private static final long serialVersionUID = -1558569760866186534L;
+
+    private Integer code;
+
+    private String message;
+
+    private T data;
+
+    public ResultResponse() {
+    }
+
+    public ResultResponse(T data) {
+        this.data = data;
+        this.code = SUCCESS_CODE;
+        this.message = SUCCESS_MESSAGE;
+    }
+
+    public ResultResponse(Integer code, String message) {
+        this.code = code;
+        this.message = message;
+        this.data = null;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public static ResultResponse SUCCESS() {
+        return new ResultResponse(SUCCESS_CODE, SUCCESS_MESSAGE);
+    }
+
+    public static <T extends Serializable> ResultResponse<T> SUCCESS(T data) {
+        return new ResultResponse<T>(data);
+    }
+
+    public static <T extends Serializable> ResultResponse<T> ERROR(Integer code, String message) {
+        return new ResultResponse<T>(code, message);
+    }
+
+    public static <T extends Serializable> ResultResponse<T> ERROR() {
+        return new ResultResponse<T>(ERROR_CODE_UNKNOWN, ERROR_MESSAGE_UNKNOWN);
+    }
+}
